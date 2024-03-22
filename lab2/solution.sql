ee29c0399b5531b4875e2fc494d740bc6a336bf0
Pkild <44123639+Pkild@users.noreply.github.com>
2024-03-22 13:13:03 +0300

    Update solution

--- lab2/solution.sql
+++ lab2/solution.sql
@@ -21,3 +21,19 @@ select sum(i0) from n33491_410_schema_lab1.n33491_410_check;
 select avg(i0) from n33491_410_schema_lab1.n33491_410_check;
 select min(i0) from n33491_410_schema_lab1.n33491_410_check;
 select max(i0) from n33491_410_schema_lab1.n33491_410_check;
+
+-- 9 --
+select * from n33491_4_10_schema_lab1.n33491_4_10_table_i5 order by i0 asc;
+
+-- 10 -- 
+select * from n33491_4_10_schema_lab1.n33491_4_10_table_i5 order by i0 desc limit 4;
+
+-- 11.2 --
+insert into n33491_4_10_schema_lab1.n33491_4_10_check values (6, '52', '6', '05-05-5555', '05-05-5555', '/x55555555');
+insert into n33491_4_10_schema_lab1.n33491_4_10_check values (4, '55', '7', '05-05-5555', '05-05-5555', '/x55555555');
+insert into n33491_4_10_schema_lab1.n33491_4_10_check values (5, '53', '5', '05-05-5555', '05-05-5555', '/x55555555');
+insert into n33491_4_10_schema_lab1.n33491_4_10_check values (7, '55', '8', '05-05-5555', '05-05-5555', '/x55555555');
+select sum(i0), i4 from n33491_4_10_schema_lab1.n33491_4_10_check group by i4;
+
+-- 12 --
+with t1 as (select sum(i0) as i0, i4 from n33491_4_10_schema_lab1.n33491_4_10_check group by i4) select * from t1 where i0 > 2;
