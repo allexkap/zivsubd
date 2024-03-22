2c077d7005f7455ebf9d54229b93f1bc9d34ac18
allexkap <86747947+allexkap@users.noreply.github.com>
2024-03-22 14:18:12 +0300

    Update values.sql

--- lab2/values.sql
+++ lab2/values.sql
@@ -28,3 +28,7 @@ insert into n33491_410_schema_lab1.n33491_410_check values (2, '22', '2', '02-02
 insert into n33491_410_schema_lab1.n33491_410_check values (3, '33', '3', '03-03-3333', '03-03-3333', '/x33333333');
 insert into n33491_410_schema_lab1.n33491_410_check values (4, '44', '4', '04-04-4444', '04-04-4444', '/x44444444');
 insert into n33491_410_schema_lab1.n33491_410_check values (5, '55', '5', '05-05-5555', '05-05-5555', '/x55555555');
+insert into n33491_410_schema_lab1.n33491_410_check values (6, '66', '6', '06-06-6666', '06-06-6666', '/x00000000');
+insert into n33491_410_schema_lab1.n33491_410_check values (7, '77', '7', '07-07-7777', '07-07-7777', '/x00000000');
+insert into n33491_410_schema_lab1.n33491_410_check values (8, '88', '8', '08-08-8888', '08-08-8888', '/x00000000');
+insert into n33491_410_schema_lab1.n33491_410_check values (9, '99', '9', '09-09-9999', '09-09-9999', '/x00000000');
