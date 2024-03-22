5ebb14632cbc5dd33e19692f072ad4f1a2d88d56
Pkild <44123639+Pkild@users.noreply.github.com>
2024-03-22 14:26:21 +0300

    Merge pull request #1 from Pkild/patch-1
    Added solutions to tasks 9-12 in lab2
    Co-authored-by: allexkap

--- lab2/solution.sql
+++ lab2/solution.sql
@@ -1,23 +1,35 @@
 -- 2 --
-update n33491_410_schema_lab1.n33491_410_p2k set i2='3' where i0 < (select avg(i0) from n33491_410_schema_lab1.n33491_410_check);
+update n33491_410_schema_lab1.n33491_410_p2k set c2='3' where c0 < (select avg(c0) from n33491_410_schema_lab1.n33491_410_check);
 
 -- 3 --
-delete from n33491_410_schema_lab1.n33491_410_check where i2 in (select i2 from n33491_410_schema_lab1.n33491_410_table_i5 where i0<3);
+delete from n33491_410_schema_lab1.n33491_410_check where c2 in (select c2 from n33491_410_schema_lab1.n33491_410_table_i5 where c0<3);
 
 -- 4 --
-select i0, i2, i3 from n33491_410_schema_lab1.n33491_410_table_i5;
+select c0, c2, c3 from n33491_410_schema_lab1.n33491_410_table_i5;
 
 -- 5 --
-select * from n33491_410_schema_lab1.n33491_410_table_i5 where i0<3;
+select * from n33491_410_schema_lab1.n33491_410_table_i5 where c0<3;
 
 -- 6 --
 select * from n33491_410_schema_lab1.n33491_410_unique, n33491_410_schema_lab1.n33491_410_p2k;
 
 -- 7 --
-select * from n33491_410_schema_lab1.n33491_410_check where i2 in (select i2 from n33491_410_schema_lab1.n33491_410_table_i5 where i0<3);
+select * from n33491_410_schema_lab1.n33491_410_check where c2 in (select c2 from n33491_410_schema_lab1.n33491_410_table_i5 where c0<3);
 
 -- 8 --
-select sum(i0) from n33491_410_schema_lab1.n33491_410_check;
-select avg(i0) from n33491_410_schema_lab1.n33491_410_check;
-select min(i0) from n33491_410_schema_lab1.n33491_410_check;
-select max(i0) from n33491_410_schema_lab1.n33491_410_check;
+select sum(c0) from n33491_410_schema_lab1.n33491_410_check;
+select avg(c0) from n33491_410_schema_lab1.n33491_410_check;
+select min(c0) from n33491_410_schema_lab1.n33491_410_check;
+select max(c0) from n33491_410_schema_lab1.n33491_410_check;
+
+-- 9 --
+select * from n33491_410_schema_lab1.n33491_410_table_i5 order by c0 asc;
+
+-- 10 -- 
+select * from n33491_410_schema_lab1.n33491_410_table_i5 order by c0 desc limit 4;
+
+-- 11 --
+select sum(c0), c5 from n33491_410_schema_lab1.n33491_410_check group by c5;
+
+-- 12 --
+with t1 as (select sum(c0) as c0, c5 from n33491_410_schema_lab1.n33491_410_check group by c5) select * from t1 where c0 > 2;
